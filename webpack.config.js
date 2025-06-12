129be577be9014a239631290c44bb769fb4557a0
Deenaism24 <160231053+Deenaism24@users.noreply.github.com>
2025-06-12 12:01:41 +0300

    dist->docs

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'docs'),
-    publicPath: '/',
+    publicPath: '/Auction/', // <-- Имя репозитория
     clean: true,
   },
   devServer: {
